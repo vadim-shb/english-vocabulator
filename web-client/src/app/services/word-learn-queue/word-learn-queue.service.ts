8df3a3a7d3b7f113a5d606116ab0e92dc1e94951
Vadim Shabanov <vadim.shabanov@gmail.com>
2017-01-18 12:39:08 +0300

    Change words learn order.

--- web-client/src/app/services/word-learn-queue/word-learn-queue.service.ts
+++ web-client/src/app/services/word-learn-queue/word-learn-queue.service.ts
@@ -8,6 +8,7 @@ class WordRepeat {
   stepsAfterLastFail: number;
   successRepeatTimes: number;
   repeatsQuantity: number;
+  repeatImportanceIndex: number;
 }
 
 class LearnableWord {
@@ -15,21 +16,22 @@ class LearnableWord {
   stepsAfterLastTest: number;
   successTimes: number;
   failTimes: number;
-  index: number;
+  learnImportanceIndex: number;
 }
 
 @Injectable()
 export class WordLearnQueueService {
 
-  private allImportances = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  private importances = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  // Words are stored in the buckets by importance.
-  private wordsToLearn: Array<LearnableWord[]> = [];
-  private wordsRepeats: Array<WordRepeat[]> = [];
+  private wordsToLearn: Array<LearnableWord> = [];
+  private wordsRepeats: Array<WordRepeat> = [];
 
-  private importanceSum :number;
   constructor(private wordService: WordService) {
   }
+
+  private lastTestedWords = {
+    lastWordId: 0,
+    beforeLastWordId: 0
+  };
 
 //======================================================================================================================
 //====================================================== setWords ======================================================
@@ -39,32 +41,19 @@ export class WordLearnQueueService {
     this.cleanWords();
 
     words.forEach(word => {
-      this.wordsToLearn[word.importance].push({
+      this.wordsToLearn.push({
         word: word,
         stepsAfterLastTest: 0,
         successTimes: 0,
         failTimes: 0,
-        index: 1
+        learnImportanceIndex: 1
       });
     });
-
-    this.dropUnusedImportances();
   }
 
   private cleanWords() {
-    this.allImportances.forEach(i => {
-      this.wordsToLearn[i] = [];
-      this.wordsRepeats[i] = [];
-    });
-  }
-
-  private dropUnusedImportances() {
-    this.importances = this.wordsToLearn
-      .filter(bucket => bucket.length > 0)
-      .map(bucket => bucket[0].word.importance)
-      .filter(importance => importance != 0)  // drop the words with zero importance.
-      .sort();
-    this.importanceSum = this.importances.reduce((a, b) => a + b);
+    this.wordsToLearn = [];
+    this.wordsRepeats = [];
   }
 
 //======================================================================================================================
@@ -82,28 +71,56 @@ export class WordLearnQueueService {
   }
 
   private getWordToLearn(): Word {
-    let importances = this.randomImportanceBucketsQueue();
-    console.log(importances);
-    for (let i = 0; i < importances.length; i++) {
-      let importance = importances[i];
-      let wordsToLearn = this.wordsToLearn[importance];
-      let randomIndex = Math.floor(Math.random() * (wordsToLearn.length));
-      return wordsToLearn[randomIndex].word;
+    this.sortWordsToLearnImportance();
+
+    for (let i = 0; i < this.wordsToLearn.length; i++) {
+      if (this.trueWithChance(70)) {
+        if (this.checkIfNotOneOfLastWords(this.wordsToLearn[i].word)) {
+          return this.wordsToLearn[i].word;
+        }
+      }
+    }
+    // the same, but drop 70% chances
+    for (let i = 0; i < this.wordsToLearn.length; i++) {
+      if (this.checkIfNotOneOfLastWords(this.wordsToLearn[i].word)) {
+        return this.wordsToLearn[i].word;
+      }
     }
   }
 
   private tryRepeat(): Word | undefined {
-    let importances = this.randomImportanceBucketsQueue();
-    for (let i = 0; i < importances.length; i++) {
-      let importance = importances[i];
-      let wordRepeatsList = this.wordsRepeats[importance];
-      for (let j = 0; j < wordRepeatsList.length; j++) {
-        let wordRepeat = wordRepeatsList[j];
+    this.sortWordsRepeatsByImportance();
+
+    for (let i = 0; i < this.wordsRepeats.length; i++) {
+      let wordRepeat = this.wordsRepeats[i];
+      if (this.checkIfNotOneOfLastWords(wordRepeat.word)) {
         if (this.isAppropreateToRepeat(wordRepeat)) {
           return wordRepeat.word;
         }
       }
     }
+  }
+
+  private sortWordsToLearnImportance(): void {
+    this.wordsToLearn.sort((learnableWord1, learnableWord2) => {
+      return learnableWord2.learnImportanceIndex - learnableWord1.learnImportanceIndex
+    });
+  }
+
+  private sortWordsRepeatsByImportance(): void {
+    this.wordsRepeats.sort((wordRepeat1, wordRepeat2) => {
+      return wordRepeat2.repeatImportanceIndex - wordRepeat1.repeatImportanceIndex
+    });
+  }
+
+  private checkIfNotOneOfLastWords(word: Word): boolean {
+    if (this.wordsToLearn.length >= 3) {
+      return word.id != this.lastTestedWords.lastWordId && word.id != this.lastTestedWords.beforeLastWordId;
+    }
+    if (this.wordsToLearn.length >= 2) {
+      return word.id != this.lastTestedWords.lastWordId;
+    }
+    return true;
   }
 
   private isAppropreateToRepeat(wordRepeat: WordRepeat): boolean {
@@ -121,21 +138,6 @@ export class WordLearnQueueService {
 
   private trueWithChance(chancePerCent: number): boolean {
     return (Math.random() * 100) < chancePerCent;
-  }
-
-
-
-  private randomImportanceBucketsQueue(): number[] {
-    let resultImportances = [];
-    let leftoverImportances = this.importances.slice();
-    let importanceSum = this.importanceSum;
-    this.importances.forEach(() => {
-      let randomStepImportance = this.chooseRrandomStepImportance(leftoverImportances, importanceSum);
-      importanceSum -= randomStepImportance;
-      resultImportances.push(randomStepImportance);
-      leftoverImportances = leftoverImportances.filter(importance => importance != randomStepImportance);
-    });
-    return resultImportances;
   }
 
   private chooseRrandomStepImportance(importances, importanceSum): number {
@@ -157,38 +159,99 @@ export class WordLearnQueueService {
 
   countAnswer(testResume: WordKnowledgeTestResume) {
     this.wordService.getWord(testResume.wordId).first().subscribe(word => {
+      this.changeLastTestedWords(word.id);
       this.increaseSteps(word);
       if (testResume.testResult != WordKnowledgeTestResultType.KNOW) {
         this.processFail(word);
       } else {
         this.processSuccess(word);
       }
+      this.recalculateIndexes();
     });
   }
 
+  private recalculateIndexes() {
+    this.wordsToLearn.forEach(wordToLearn => {
+      this.recalculateLearnImportanceIndex(wordToLearn, this.wordsToLearn.length);
+    });
+    // (!) repeatImportanceIndex depends on learnImportanceIndex
+    this.wordsRepeats.forEach(wordRepeat => {
+      this.recalculateRepeatImportanceIndex(wordRepeat);
+    });
+  }
+
+  private recalculateLearnImportanceIndex(learnableWord: LearnableWord, wordListLength: number): void {
+    let stepsIndex = learnableWord.stepsAfterLastTest - 0.9 * wordListLength;
+    if (stepsIndex < 1) stepsIndex = 1;
+
+    let failIndex = (learnableWord.failTimes + 3) * 4 - learnableWord.successTimes;
+    if (failIndex < 1) failIndex = 0.5; // word is learned.
+
+    learnableWord.learnImportanceIndex = learnableWord.word.importance * stepsIndex * failIndex;
+  }
+
+  private recalculateRepeatImportanceIndex(wordRepeat: WordRepeat): void {
+    let learnableWord = this.getLearnableWord(wordRepeat.word);
+    let learnImportanceIndex = learnableWord.learnImportanceIndex;
+
+    let stepsAfterFailIndex = 1;
+
+    if (wordRepeat.stepsAfterLastFail >= 3) {
+      stepsAfterFailIndex = wordRepeat.stepsAfterLastFail;
+    }
+    if (wordRepeat.stepsAfterLastFail >= 6) {
+      stepsAfterFailIndex = wordRepeat.stepsAfterLastFail / 1.5;
+    }
+    if (wordRepeat.stepsAfterLastFail >= 9) {
+      stepsAfterFailIndex = wordRepeat.stepsAfterLastFail / 2;
+    }
+
+    let successRepeatsIndex;
+    if (wordRepeat.successRepeatTimes === 0) {
+      successRepeatsIndex = 2;
+    }
+    if (wordRepeat.successRepeatTimes === 1) {
+      successRepeatsIndex = 1 / 2;
+    }
+    if (wordRepeat.successRepeatTimes === 2) {
+      successRepeatsIndex = 1 / 4;
+    }
+    if (wordRepeat.successRepeatTimes === 3) {
+      successRepeatsIndex = 1 / 7;
+    }
+    if (wordRepeat.successRepeatTimes >= 4) {
+      successRepeatsIndex = 1 / 10;
+    }
+
+    wordRepeat.repeatImportanceIndex = learnImportanceIndex * stepsAfterFailIndex * successRepeatsIndex;
+  }
+
+  private changeLastTestedWords(testedWordId) {
+    this.lastTestedWords.beforeLastWordId = this.lastTestedWords.lastWordId;
+    this.lastTestedWords.lastWordId = testedWordId;
+  }
+
   private increaseSteps(word: Word) {
-    this.importances.forEach(i => {
-      this.wordsRepeats[i].forEach(wordRepeat => {
-        wordRepeat.stepsAfterLastFail++;
-      });
-      this.wordsToLearn[i].forEach(learnableWord => {
-        if (learnableWord.word.id === word.id) {
-          learnableWord.stepsAfterLastTest = 0;
-        } else {
-          learnableWord.stepsAfterLastTest++;
-        }
-      });
+    this.wordsRepeats.forEach(wordRepeat => {
+      wordRepeat.stepsAfterLastFail++;
+    });
+    this.wordsToLearn.forEach(learnableWord => {
+      if (learnableWord.word.id === word.id) {
+        learnableWord.stepsAfterLastTest = 0;
+      } else {
+        learnableWord.stepsAfterLastTest++;
+      }
     });
   }
 
   private getLearnableWord(word: Word): LearnableWord | undefined {
-    return this.wordsToLearn[word.importance]
+    return this.wordsToLearn
       .filter(wordToLearn => wordToLearn.word.id === word.id)
       .pop()
   }
 
   private getWordRepeat(word: Word): WordRepeat | undefined {
-    return this.wordsRepeats[word.importance]
+    return this.wordsRepeats
       .filter(wordRepeat => wordRepeat.word.id === word.id)
       .pop()
   }
@@ -210,8 +273,8 @@ export class WordLearnQueueService {
   }
 
   private removeWordRepeat(wordRepeat: WordRepeat) {
-    let index = this.wordsRepeats[wordRepeat.word.importance].indexOf(wordRepeat);
-    this.wordsRepeats[wordRepeat.word.importance].splice(index, 1);
+    let index = this.wordsRepeats.indexOf(wordRepeat);
+    this.wordsRepeats.splice(index, 1);
   }
 
   private processFail(word: Word) {
@@ -227,11 +290,12 @@ export class WordLearnQueueService {
       wordRepeat.repeatsQuantity += 2;
 
     } else {
-      this.wordsRepeats[word.importance].push({
+      this.wordsRepeats.push({
         word: word,
         stepsAfterLastFail: 0,
         successRepeatTimes: 0,
-        repeatsQuantity: 3
+        repeatsQuantity: 3,
+        repeatImportanceIndex: 0
       })
     }
   }
